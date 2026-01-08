38812e69cd95c4d6c02927ae33ccb0a877d91e6c
Mena Farouk <60456761+menafarouk@users.noreply.github.com>
2026-01-08 23:32:29 +0200

    testing js

---
+++ root/script.js
@@ -0,0 +1,8 @@
+console.log("test-script");
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (document.body.classList.contains('salla-965561406')) {
+    console.log("test2");
+  }
+});

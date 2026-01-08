f3ff5aa193895356cc690d748d6d0b33af910bc5
Mena Farouk <60456761+menafarouk@users.noreply.github.com>
2026-01-08 23:43:02 +0200

    testing js

--- root/script.js
+++ root/script.js
@@ -1,9 +1,9 @@
-console.log("test-script");
+// console.log("test-script");
 
 
-document.addEventListener('DOMContentLoaded', () => {
-  const ogUrl = document.querySelector('meta[property="og:url"]')?.getAttribute('content');
-  if (ogUrl === 'https://salla.sa/take.trend') {
-    console.log("test2");
-  }
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//   const ogUrl = document.querySelector('meta[property="og:url"]')?.getAttribute('content');
+//   if (ogUrl === 'https://salla.sa/take.trend') {
+//     console.log("test2");
+//   }
+// });
